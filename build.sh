ca31bd6501a76bb58afee072d939d3523d491356
songhw <lhammering@gmail.com>
2018-03-10 14:52:38 +0800

    :tada:feat: add auto copy template to test env

--- build.sh
+++ build.sh
@@ -8,20 +8,42 @@
 
 # 打包成功后自动拷贝
 
+month=$(date +%m)
+host=songhw@118.190.101.187
+www=www@10.30.214.232:/home/www/
+wx=yindou_02/application/views/wx/events/2018/$month/
+h5=yindou_02/application/views/h5/events/2018/$month/
+web=yindou_02/application/views/web/events/2018/$month/
+
 if [ ! $1 ]; then
     echo -e "\033[33m > 🚨  warning: 请输入参数@param1 @param2 (@param1:web/app/webeg/appeg; @param2:--no-minify) \033[0m"
 else
     rm -rf dist/ && \
     git co *.tpl && \
     if [[ $1 == "web" && ! $2 ]]; then
+        files=./src/web/*.tpl
         npm run webbuildmin && \
-        python ./bin/autocopy.py $1
+        python ./bin/autocopy.py $1 && \
+        # see https://linux.cn/article-7456-1.html && http://man.linuxde.net/scp
+        scp -P 38 -rq -o ProxyCommand='ssh '$host' -p 21222 -A -W %h:%p' $files $www$web
+        for file in ${files} 
+        do    
+            temp_file=`basename $file`    
+            echo -e "\n\033[32m🚚  Scp $temp_file to $web done \033[0m\n" 
+        done
     elif [[ $1 == "web" && $2 == "--no-minify" ]]; then
         npm run webbuild && \
         python ./bin/autocopy.py web
     elif [[ $1 == "app" && ! $2 ]]; then
+        files=./src/app/*.tpl
         npm run appbuildmin && \
-        python ./bin/autocopy.py $1
+        python ./bin/autocopy.py $1 && \
+        scp -P 38 -rq -o ProxyCommand='ssh '$host' -p 21222 -A -W %h:%p' $files $www$wx
+        for file in ${files} 
+        do    
+            temp_file=`basename $file`    
+            echo -e "\n\033[32m🚚  Scp $temp_file to $wx done \033[0m\n" 
+        done
     elif [[ $1 == "app" && $2 == "--no-minify" ]]; then
         npm run appbuild && \
         python ./bin/autocopy.py $1
